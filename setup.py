91c5ae80df80bcae988f76ba99a3d26bef9ebcae
Nicolas Marin <josue.marin1729@gmail.com>
2021-02-12 04:09:23 -0500

    added long description lo setup

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
+
+with open("README.md","r") as fh:
+    long_description = fh.read()  
+
 setup(
     name = 'nxcurve',         # How you named your package folder (MyLib)
     version = '0.6',      # Start with a small number and increase it with every change you make
     description = 'draws RNS,QNX and BNX curves and their auc',   # Give a short description about your library
     py_modules = ['nxcurve'],
     package_dir={'':'src'},
+    long_description=long_description,
   
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
- 
     author = 'Nicolas Marin',                   # Type in your name
     author_email = 'josue.marin1729@gmail.com',      # Type in your E-Mail
     url = 'https://github.com/tardigrade33/nxcurve',   # Provide either the link to your github or to your website
