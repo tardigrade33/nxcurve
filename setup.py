c8f22a965f60f8515fe9115f062cb07fab4981b7
Nicolas Marin <josue.marin1729@gmail.com>
2021-02-18 01:59:47 -0500

    discarting some files that shoulnd be there

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name = 'nxcurve',         # How you named your package folder (MyLib)
-    version = '0.6.1',      # Start with a small number and increase it with every change you make
+    version = '0.6.2',      # Start with a small number and increase it with every change you make
     description = 'draws RNS,QNX and BNX curves and their auc',   # Give a short description about your library
     py_modules = ['nxcurve'],
     package_dir={'':'src'},
@@ -16,7 +16,6 @@ setup(
     author = 'Nicolas Marin',                   # Type in your name
     author_email = 'josue.marin1729@gmail.com',      # Type in your E-Mail
     url = 'https://github.com/tardigrade33/nxcurve',   # Provide either the link to your github or to your website
-    #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
     keywords = ['RNX', 'qualitycurve', 'QNX', 'BNX'],   # Keywords that define your package best
  
     install_requires=[            # I get to this in a second
